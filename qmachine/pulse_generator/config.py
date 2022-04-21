6cf97d7e0f3b3759fe5bd0c5928d2a210e98efc6
Fabrizio <fabrizio.berritta@nbi.ku.dk>
2022-04-21 13:41:08 +0200

    made a branch for the pulse generator work and uploaded the currently used files.

---
+++ qmachine/pulse_generator/config.py
@@ -0,0 +1,238 @@
+import numpy as np
+#router ip 192.168.15.128
+
+readout_length = 1000
+
+Q1_readout_frequency = 156390000
+Q2_readout_frequency = 129291200
+
+#(156759540.0, 129291200.0)
+
+config = {
+
+    "version": 1,
+
+    "controllers": {
+        "con1": {
+
+            "analog_outputs": {
+                1: {"offset": 0.0},  # Readout
+                3: {"offset": 0.0},  # Q1_R BNC12/j9
+                2: {"offset": 0.0},  # Q1_L BNC10/j8
+            },
+
+            "digital_outputs": {
+                1: {},
+            },
+
+            "analog_inputs": {
+                1: {"offset": 0.21164},
+                2: {"offset": 0.0},
+            },
+
+        }
+    },
+
+    "elements": {
+
+        "Q1_R": {
+            "singleInput": {
+                "port": ("con1", 3)
+            },
+            'intermediate_frequency': 0e6,
+            'hold_offset':{'duration': 100},
+            'operations': {
+                'CW': "CW",
+            },
+        },
+
+        "Q1_L": {
+            "singleInput": {
+                "port": ("con1", 2)
+            },
+            'intermediate_frequency': 0e6,
+            'hold_offset':{'duration': 100},
+            'operations': {
+                'CW': "CW",
+            },
+        },
+
+        # "Q2_R": {
+        #     "singleInput": {
+        #         "port": ("con1", 4)
+        #     },
+        #     'intermediate_frequency': 0e6,
+        #     'operations': {
+        #         'CW': "CW",
+        #     },
+        # },
+
+        # "Q2_L": {
+        #     "singleInput": {
+        #         "port": ("con1", 5)
+        #     },
+        #     'intermediate_frequency': 0e6,
+        #     'operations': {
+        #         'CW': "CW",
+        #     },
+        # },
+
+        "Q1_readout": {
+            "singleInput": {
+              "port": ("con1", 1)
+              },
+            "intermediate_frequency": Q1_readout_frequency,
+            "operations": {
+                "readout_pulse_0_2": "readout_pulse_0_2",
+                "readout_pulse_0_1": "readout_pulse_0_1",
+                "readout_pulse_0_05": "readout_pulse_0_05",
+                "readout_pulse_0_01": "readout_pulse_0_01",
+                "readout_pulse_0_0001": "readout_pulse_0_0001",
+            },
+            "outputs": {"out1": ("con1", 1)},
+            "time_of_flight": 180+190-2,
+            "smearing": 0,
+        },
+
+        "Q2_readout": {
+            "singleInput": {
+                "port": ("con1", 1)
+            },
+            "intermediate_frequency": Q2_readout_frequency,
+            "operations": {
+                "readout_pulse_1_danger": "readout_pulse_1_danger",
+                "readout_pulse_0_05": "readout_pulse_0_05",
+                "readout_pulse_0_2": "readout_pulse_0_2",
+            },
+            "outputs": {"out1": ("con1", 1)},
+            "time_of_flight": 180+190-2,
+            "smearing": 0,
+        },
+
+    },
+
+    "pulses": {
+
+        "CW": {
+            "operation": "control",
+            "length": 100,
+            "waveforms": {"single": "const_wf"},
+        },
+
+        "readout_pulse_1_danger": {
+            "operation": "measurement",
+            "length": readout_length,
+            "waveforms": {"single": "readout_wf_1_danger"},
+            "integration_weights": {
+                "cos": "cos",
+                "sin": "sin",
+            },
+            'digital_marker': 'ON'
+        },
+
+        "readout_pulse_0_2": {
+            "operation": "measurement",
+            "length": readout_length,
+            "waveforms": {"single": "readout_wf_0_2"},
+            "integration_weights": {
+                "cos": "cos",
+                "sin": "sin",
+            },
+            'digital_marker': 'ON'
+        },
+        "readout_pulse_0_1": {
+            "operation": "measurement",
+            "length": readout_length,
+            "waveforms": {"single": "readout_wf_0_1"},
+            "integration_weights": {
+                "cos": "cos",
+                "sin": "sin",
+            },
+            'digital_marker': 'ON'
+        },
+        "readout_pulse_0_05": {
+            "operation": "measurement",
+            "length": readout_length,
+            "waveforms": {"single": "readout_wf_0_05"},
+            "integration_weights": {
+                "cos": "cos",
+                "sin": "sin",
+            },
+            'digital_marker': 'ON'
+        },
+        "readout_pulse_0_01": {
+            "operation": "measurement",
+            "length": readout_length,
+            "waveforms": {"single": "readout_wf_0_01"},
+            "integration_weights": {
+                "cos": "cos",
+                "sin": "sin",
+            },
+            'digital_marker': 'ON'
+        },
+        "readout_pulse_0_001": {
+            "operation": "measurement",
+            "length": readout_length,
+            "waveforms": {"single": "readout_wf_0_001"},
+            "integration_weights": {
+                "cos": "cos",
+                "sin": "sin",
+            },
+            'digital_marker': 'ON'
+        },
+        "readout_pulse_0_0001": {
+            "operation": "measurement",
+            "length": readout_length,
+            "waveforms": {"single": "readout_wf_0_0001"},
+            "integration_weights": {
+                "cos": "cos",
+                "sin": "sin",
+            },
+            'digital_marker': 'ON'
+        },
+
+
+
+    },
+
+    "waveforms": {
+
+        "const_wf": {"type": "constant", "sample": 0.1}, #previously this "sample": 0.05*7.063 
+
+        "zero_wf": {"type": "constant", "sample": 0.0},
+
+        "readout_wf_1_danger": {"type": "constant", "sample": 1},
+
+        "readout_wf_0_2": {"type": "constant", "sample": 0.2},
+
+        "readout_wf_0_1": {"type": "constant", "sample": 0.1},
+
+        "readout_wf_0_05": {"type": "constant", "sample": 0.05},
+
+        "readout_wf_0_01": {"type": "constant", "sample": 0.01},
+
+        "readout_wf_0_001": {"type": "constant", "sample": 0.001},
+
+        "readout_wf_0_0001": {"type": "constant", "sample": 0.0001},
+
+    },
+
+    "digital_waveforms": {
+        "ON": {"samples": [(1, 0)]}
+    },
+
+    "integration_weights": {
+
+        "cos": {
+            "cosine": [(1.0, readout_length)],
+            "sine": [(0.0, readout_length)],
+        },
+
+        "sin": {
+            "cosine": [(0.0, readout_length)],
+            "sine": [(1.0, readout_length)],
+        },
+
+    },
+
+}
---
+++ qmachine/pulse_generator/pulse_generator.py
@@ -0,0 +1,290 @@
+# from config import config
+from qm.qua import program,for_,stream_processing,declare,declare_stream,wait,measure,play,save,fixed,demod,ramp,amp,if_,elif_,else_,align
+from qm.QuantumMachinesManager import QuantumMachinesManager
+from qm import SimulationConfig
+
+import pandas
+import numpy as np
+import matplotlib.pyplot as plt
+import pprint
+
+class pulser():
+    def __init__(self,config) -> None:
+        self.config=config
+
+        self.channels=['ch1','ch2']
+        
+        self.channel_dict={'ch1':'Q1_L' , 'ch2':'Q1_R', 'm1':'Q2_readout'}
+        self.meas_dict={'full':demod.full , 'sliced':demod.sliced}
+
+        self.action_dict={'hold':self._make_wait , 'step':self._make_step , 'ramp':self._make_ramp, 'meas':self._make_meas}
+        self.cw_conversion=1
+
+        # self.open_qm(config)
+
+    def simulate_pulse(self,pulsing_sequence,simulation_time):
+        if not hasattr(self,'qm'):
+            self.open_qm(self.config)
+        job = self.qm.simulate(pulsing_sequence, SimulationConfig(simulation_time))
+        samples = job.get_simulated_samples()
+        samples.con1.plot()
+        # self._plot_simulated_pulse(samples)
+        return samples
+    
+    def _plot_simulated_pulse(self,samples,meas_channel=1):
+        fig,ax=plt.subplots()
+        ax2=ax.twinx()
+        measurement_on_points=np.where(samples.con1.analog[str(meas_channel)]!=0)
+        ax.plot(samples.con1.analog['3'])
+        ax.plot(samples.con1.analog['2'])
+        ax2.plot(measurement_on_points,np.ones(len(measurement_on_points)),'.')
+        ax2.set_ylim(0,1.05)
+        
+        
+
+
+    def open_qm(self,config):
+        #load the quantum machine this should probably only be done when simulating/performing the pulse
+        self.config=config
+        self.qmm = QuantumMachinesManager(host='192.168.15.128',port=80)
+        self.qmm.close_all_quantum_machines()
+        self.qm = self.qmm.open_qm(config)
+        print(f"quantum machine opened with channels {list(self.config['elements'].keys())}")
+        print(f"default value for CW is: {self.config['waveforms']['const_wf']['sample']}")
+        self.cw_conversion=1/self.config['waveforms']['const_wf']['sample']
+
+
+    def _build_seq(self,actions):
+        with program() as sequence:
+            if 'm1' in actions['channels'] or 'm2' in actions['channels']: #changes here
+                self._declare_measurements(actions)
+
+            if 'looped' in actions.keys(): #changes here
+                loop_indexes,looped_actions = self._declare_loops(actions) #declare loop variables and return dict with loop indexes. and arrays to be looped over.       
+                self._run_loops(loop_indexes=loop_indexes,actions=looped_actions,loop_nr=0) #run_loops will run a single with for_ loop and call itself within this loop. run_loops also will can _run_all_actions.
+            else:
+                self._run_all_actions(actions) #if no loops are to be done it will just run all the actions.
+        return sequence
+
+    def _run_loops(self,loop_indexes,actions,loop_nr=0):
+        #check if we are still supposed to loop more
+        if loop_nr<len(loop_indexes): 
+            loop_index=loop_indexes[str(loop_nr)]
+            with for_(loop_index,0,loop_index<actions['looped'][int(loop_nr)],loop_index+1): #setup the loop
+                loop_nr+=1
+                self._run_loops(loop_indexes,actions,loop_nr) #call itself again going a loop deeper each time.
+        else: #if no more looping.
+            self._run_all_actions(actions,loop_indexes)
+
+    def _run_all_actions(self,actions,loop_indexes=None): #this will just run all the actions
+        for step,row_actions in actions['steps'].items():
+            align(*[self.channel_dict[channel] for channel in row_actions.keys()])
+            for key,channel_actions in row_actions.items(): #key should be changed to channel here
+                if ('ch' in key or 'm' in key):
+                    if channel_actions['looped']: #if a variable is looped it will have the different values it takes declared as an array on the OPX, this will tell python what that OPX variable is.
+                        channel_actions['action_variables'][channel_actions['looper']]=channel_actions['loop_param'][loop_indexes[channel_actions['loop_index']]]
+
+                    self._perform_action(channel_actions) 
+
+    def _declare_loops(self,actions):
+        loop_indexes={str(i):declare(int) for i in range(len(actions['looped']))} #declares all the loop indexes
+        for step,row_actions in actions['steps'].items():
+            for key,channel_actions in row_actions.items():
+                if 'ch' in key:
+                    if channel_actions['looped']: #declare only looped parameters
+                        channel_actions['loop_param']=declare(fixed,value=channel_actions['action_variables'][channel_actions['looper']])
+        return loop_indexes,actions
+
+    def _declare_measurements(self,actions):
+        k=1
+        for step,row_actions in actions['steps'].items():
+            for key,channel_actions in row_actions.items(): 
+                if 'm' in key:
+                    channel_actions['action_variables']['save_I']=declare(fixed)
+                    channel_actions['action_variables']['save_Q']=declare(fixed)
+
+                    channel_actions['action_variables']['I_name']=f'I_{k}'
+                    channel_actions['action_variables']['Q_name']=f'Q_{k}'
+                    k+=1
+                    
+
+                    
+    def _perform_action(self,action):
+        self.action_dict[action['action']](action['channel'],action['action_variables'])
+
+    #the different actions implemented, this is where the QUA functions are being ran.
+    def _make_wait(self,channel,variables):
+        wait(variables['time'],self.channel_dict[channel])
+
+    def _make_ramp(self,channel,variables):
+        # print(f'rate is {variables["rate"]}V/ns with time {variables["time"]*4}ns')
+        play(ramp(variables['rate']), self.channel_dict[channel], duration=variables['time'])
+
+    def _make_step(self,channel,variables):
+        play('CW'*amp(variables['step_value']), self.channel_dict[channel], duration=variables['time'])
+
+    def _make_meas(self,channel,variables):
+        measure(variables['pulse'],self.channel_dict[channel],None,
+                self.meas_dict[variables['type']]('cos',variables['save_I']), 
+                self.meas_dict[variables['type']]('sin',variables['save_Q']))
+
+
+
+
+#below is legacy for now, will return to it when above is done.
+class pulse_generator():
+    def __init__(self,config):
+        self.config=config
+        self.position={'ch1':0,'ch2':0}
+
+  
+        self.channels=['ch1','ch2']
+        
+        self.channel_dict={'ch1':'Q1_L' , 'ch2':'Q1_R'}
+
+        self.action_dict={'hold':self._make_wait , 'step':self._make_step , 'ramp':self._make_ramp, 'loop':self._make_step}
+
+        self.open_qm(config)
+
+        
+    def make_pulse_from_df(self,df):
+        self.other_cols=[i for i in df.columns.values if 'ch' not in i]
+        dfcopy=df.copy(deep=True)
+        dfcopy['time']=dfcopy['time'].apply(lambda x : int(x//4)) #converts time to clockcycles of the fpga (4ns)
+
+        
+        actions={ch : {} for ch in self.channels}
+        for channel in self.channels:
+            channel_df=dfcopy[[channel]+self.other_cols].copy(deep=True)
+
+            for index,row in channel_df.iterrows():
+                if isinstance(row[channel],(float,int)):
+                    row[channel]=[row[channel]]
+                actions[channel][str(index)]=self._identify_actions(row,channel)
+
+                self._update_position(row,channel)
+                # print(f'my position is now {self.position}')
+                # print(f"this row {row[self.channels].values} was assigned this action {actions[-1][0][0],actions[-1][1][0]}")
+        loops=self._check_loop(actions)
+        if loops['found']:
+            self._fix_loop_channel(loops['where'],actions)
+        # return actions
+        return self._build_seq(actions) , actions
+        
+    def simulate_pulse(self,pulsing_sequence,simulation_time):
+        if not hasattr(self,'qm'):
+            self.open_qm(self.config)
+        job = self.qm.simulate(pulsing_sequence, SimulationConfig(simulation_time))
+        samples = job.get_simulated_samples()
+        samples.con1.plot()
+
+
+    def open_qm(self,config):
+        #load the quantum machine this should probably only be done when simulating/performing the pulse
+        self.config=config
+        self.qmm = QuantumMachinesManager(host='192.168.15.128',port=80)
+        self.qmm.close_all_quantum_machines()
+        self.qm = self.qmm.open_qm(config)
+        print(f"quantum machine opened with channels {list(self.config['elements'].keys())}")
+        print(f"default value for CW is: {self.config['waveforms']['const_wf']['sample']}")
+        self.cw_conversion=1/self.config['waveforms']['const_wf']['sample']
+    
+
+
+    def _build_seq(self,actions):
+        # loop_present=False
+        # for ac in actions:
+        #     for a in ac:
+        #         if a[0]=='loop':
+        #             loop_present=True
+        #             loop_variables=a[2]
+        #             print('loop_present')    
+
+        with program() as sequence:
+            #declare things
+            # if loop_present:
+            #     print('im here')
+            #     self._make_the_loop(loop_variables,actions)
+            # else:
+            for i in range(len(actions['ch1'])):
+                for ch in self.channels:
+                    self._perform_action(actions[ch][str(i)])
+        return sequence
+    
+ 
+    def _perform_action(self,action):
+        self.action_dict[action['action']](action['channel'],action['action_variables'])
+
+    
+    def _update_position(self,row,channel):
+        if len(row[channel])==3: #loop
+            print('loop not implemented yet')
+
+        elif len(row[channel])==2: #ramp move
+            self.position[channel]=row[channel][1]
+            
+        else: #step or hold
+            self.position[channel]=row[channel][0]
+
+    def _check_loop(self,actions):
+        loops={'where':{ch:[] for ch in self.channels}, 'found':False}
+        for channel,di in actions.items():
+            for step,di2 in di.items():
+                if di2['action']=='loop':
+                    loops[channel].append(step)
+                    loops['found']=True
+                
+        return loops
+    
+    def _fix_loop_channel(self,where,actions):
+        print('fix the loop channel')
+
+    def _make_wait(self,channel,variables):
+        wait(variables['time'],self.channel_dict[channel])
+
+    def _make_ramp(self,channel,variables):
+        print(f'rate is {variables["rate"]}V/ns with time {variables["time"]*4}ns')
+        play(ramp(variables['rate']), self.channel_dict[channel], duration=variables['time'])
+
+    def _make_step(self,channel,variables):
+        play('CW'*amp(self.cw_conversion*variables['step_value']), self.channel_dict[channel], duration=variables['time'])
+
+    def _identify_actions(self,row,channel):
+        # actions={}
+        action=self._channel_action(channel,row[channel])
+        action_variables=self._get_variables(action,row,channel)
+        action={'action':action , 'channel':channel, 'action_variables':action_variables}
+        return action
+
+    def _get_variables(self,action,row,channel):
+        if action=='hold':
+            return {'time':row['time']}
+
+        if action=='step':
+            step=row[channel][0]-self.position[channel]
+            return {'time':row['time'] , 'step_value':step}
+
+        if action=='ramp':
+            rate=(row[channel][1]-self.position[channel])/(row['time']*4) #times 4 to account for clockcycles to ns
+            return {'rate':rate , 'time': row['time']}
+
+        if action=='loop':
+            startvalue=row[channel][0]-self.position[channel]
+            endvalue=row[channel][1]-self.position[channel]
+            steps=row[channel][2]
+            stepsize=(endvalue-startvalue)/steps
+            return {'time': row['time'] , 'stepsize':stepsize , 'start':startvalue, 'end':endvalue , 'steps':steps}
+
+
+    def _channel_action(self,channel,input):
+        if len(input)==1: #step or hold
+            if input[0]==self.position[channel]:
+                return 'hold'
+            else:
+                return 'step'    
+
+        elif len(input)==2: #ramp
+            return 'ramp'
+
+        elif len(input)==3:
+            return 'loop'
